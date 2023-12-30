b34844d1cf13ce326843c5e1b37d3093b20d8f44
alimurrofid <alimurrofid77@gmail.com>
2023-12-30 16:51:26 +0700

    feat:button back

--- resources/views/dashboard/book/create.blade.php
+++ resources/views/dashboard/book/create.blade.php
@@ -6,6 +6,11 @@
             <div class="w-full max-w-full px-3 flex-0">
                 <div class="flex flex-wrap -mx-3">
                     <div class="w-full max-w-full px-3 m-auto flex-0 lg:w-8/12">
+                        <div class="my-4">
+                            <a href="{{ route('book.index') }}" class="inline-block px-4 py-2 text-sm font-medium leading-5 text-dark transition-colors duration-150 bg-white border border-transparent rounded-lg active:bg-yellow-300 hover:bg-yellow-400 focus:outline-none focus:shadow-outline-yellow hover:text-white ni ni-bold-left">
+                                Back    
+                            </a>
+                        </div>
                         <form class="relative mb-32" style="height: 402px;" action="{{route('book.store')}}" enctype="multipart/form-data" method="POST">
                             @csrf
                             <div
