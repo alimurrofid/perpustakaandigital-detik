f283619a1b0c4d263a34be64f592528ab4a88792
alimurrofid <alimurrofid77@gmail.com>
2023-12-30 14:41:11 +0700

    feat:authorisation

--- resources/views/dashboard/partials/sidenav.blade.php
+++ resources/views/dashboard/partials/sidenav.blade.php
@@ -43,16 +43,18 @@
                 </a>
             </li>
 
-            <li class="mt-0.5 w-full">
-                <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors {{ request()->routeIs('candidate.index', 'candidate.create', 'candidate.edit') ? 'font-semibold text-slate-700 rounded-lg bg-blue-500/13' : '' }}"
-                    href="{{ route('category.index') }}">
-                    <div
-                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                        <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-books"></i>
-                    </div>
-                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Category</span>
-                </a>
-            </li>
+            @can('isAdmin')
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors {{ request()->routeIs('candidate.index', 'candidate.create', 'candidate.edit') ? 'font-semibold text-slate-700 rounded-lg bg-blue-500/13' : '' }}"
+                        href="{{ route('category.index') }}">
+                        <div
+                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-books"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Category</span>
+                    </a>
+                </li>
+            @endcan
 
         </ul>
     </div>
